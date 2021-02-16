618a06df19b63aac223e38d0b6a87d4f22eb98d6
polar.zhang <polar@xsintlg.com>
2021-02-16 16:00:00 +0800

    Use thread to perform quick sort.

---
+++ QuickThreadSorting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace QuickThreadSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr1 = CreateRandomArray(10000000);
+            var arr2 = new int[arr1.Length];
+            arr1.CopyTo(arr2, 0);
+
+            DateTime dateTime = DateTime.Now;
+            new QuickSort(true, 1000).Sort(arr2);
+            Console.WriteLine("Use Thread: {0}", (DateTime.Now - dateTime).Ticks);
+
+            dateTime = DateTime.Now;
+            new QuickSort(false).Sort(arr1);
+            Console.WriteLine(" No Thread: {0}", (DateTime.Now - dateTime).Ticks);
+
+            Console.WriteLine("Press any key to continue.");
+            Console.Read();
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            foreach(var a in arr)
+            {
+                Console.Write(a + " ");
+            }
+        }
+
+        private static int[] CreateRandomArray(int length)
+        {
+            int[] arr = new int[length];
+            for(int i = 0; i < length;i ++)
+            {
+                arr[i] = i;
+            }
+
+            return arr.OrderBy(a => Guid.NewGuid()).ToArray();
+        }
+    }
+}
---
+++ QuickThreadSorting/QuickSort.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickThreadSorting
+{
+    class QuickSort
+    {
+        private bool useThread;
+        private int length;
+
+        /// <summary>
+        /// Use thread when the to be sorted array is larger than length.
+        /// </summary>
+        /// <param name="useThread"></param>
+        /// <param name="length"></param>
+        public QuickSort(bool useThread, int length = 10000)
+        {
+            this.useThread = useThread;
+            this.length = 10;
+        }
+
+        public void Sort(int[] arr)
+        {
+            Sort(arr, 0, arr.Length - 1);
+        }
+
+        private void Sort(int[] arr, int low, int high)
+        {
+            if (low >= high)
+                return;
+
+            int p = Partition(arr, low, high);
+
+
+            if (useThread && p - 1 - low > length)
+            {
+                Task.Run(() =>
+                {
+                    Sort(arr, low, p - 1);
+                });
+            }
+            else
+            {
+                Sort(arr, low, p - 1);
+            }
+
+            if (useThread && high - p - 1 > length)
+            {
+                Task.Run(() =>
+                {
+                    Sort(arr, p + 1, high);
+                });
+            }
+            else
+            {
+                Sort(arr, p + 1, high);
+            }
+        }
+
+        private int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+            int i = low;
+            for (int j = low; j <= high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    Swap(ref arr[i], ref arr[j]);
+                    i++;
+                }
+            }
+
+            Swap(ref arr[i], ref arr[high]);
+
+            return i;
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            if (a == b) return;
+
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
